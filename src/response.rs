008b3e21681ba0af6289d35d0c066a5183d728c4
zeke <zkemmerling@gmail.com>
2024-02-03 19:23:11 -0500

    Implemented proper Response handling

--- src/response.rs
+++ src/response.rs
@@ -1,51 +1,46 @@
-use crate::prelude::*;
+// use crate::prelude::*;
 use reqwest::header::HeaderMap;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::collections::HashMap;
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 pub struct Response {
     status: u16,
     headers: Option<HashMap<String, String>>,
-    version: String,
-    body: String,
+    body: Option<Value>,
 }
 
 impl Response {
-    pub fn new(
-        status: u16,
-        headers: Option<HashMap<String, String>>,
-        version: String,
-        body: String,
-    ) -> Self {
+    pub fn new(status: u16, headers: Option<HashMap<String, String>>, body: Option<Value>) -> Self {
         Response {
             status,
-            version,
             headers,
             body,
         }
     }
 }
 
-fn build_headers(hdrs: Option<HeaderMap>) -> Result<HashMap<String, String>> {
-    let mut headers: HashMap<String, String> = HashMap::new();
+impl Response {
+    pub fn build_headers(hdrs: &HeaderMap) -> Option<HashMap<String, String>> {
+        let mut headers: HashMap<String, String> = HashMap::new();
 
-    match hdrs {
-        Some(hdrs) => {
-            for (k, v) in hdrs.iter() {
-                let k = k.to_string();
-                let v = v.to_str();
-                // Reqwest
-                if let Ok(val) = v {
-                    headers.insert(k, val.to_string());
-                } else {
-                    let e = String::from("Failed to read this fields contents");
-                    headers.insert(k, e);
-                }
+        if hdrs.is_empty() {
+            return None;
+        }
+
+        for (k, v) in hdrs.iter() {
+            let k = k.to_string();
+            let v = v.to_str();
+            // Reqwest can fail to convert a value to a String but not a key
+            if let Ok(val) = v {
+                headers.insert(k, val.to_string());
+            } else {
+                // TODO: Output error to stderr
+                let e = String::from("Failed to read this fields contents");
+                headers.insert(k, e);
             }
         }
-        // TODO: Don't think this can ever happen
-        None => {}
+        Some(headers)
     }
-    Ok(headers)
 }
--- src/writer.rs
+++ src/writer.rs
@@ -1,20 +1,59 @@
-use crate::prelude::*;
-use reqwest::blocking::Response;
-use std::{fs, path::Path};
+use crate::{prelude::*, response};
+use serde_json::Value;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
-pub fn write_file(path: &str, res: Response) -> Result<()> {
-    println!("yay");
-    // Will redirect the data to a new response file in the same folder as the request
-    let path: &str = &(path[..path.len() - 5].to_owned() + "_response.json");
-    let file_path = Path::new(path);
-    // let contents = res.bytes()?;
-    let s = res.status().to_owned();
-    // let u = res.url();
-    let h = res.headers().to_owned();
-    let a = res.remote_addr().unwrap().to_owned().to_string();
-    let v = res.version();
-    // let e = res.extensions();
-    let contents = res.text()?;
-    let result = fs::write(file_path, contents);
-    Ok(result?)
+pub fn write_file(path: &str, res: reqwest::blocking::Response) -> Result<()> {
+    // Responses are stored in an output directory
+    let request_path = Path::new(path).to_path_buf();
+    let response_path = build_file_path(request_path)?;
+
+    // Extract our response and convert to our internal struct for serializing to JSON
+    let status: u16 = res.status().as_u16();
+    let headers = response::Response::build_headers(res.headers());
+    let contents: Value = res.json()?; // Convert response body to serde_json::Value for ease
+    let resp = response::Response::new(status, headers, Some(contents)); // Pass to Response struct
+
+    let json_contents = serde_json::to_string_pretty(&resp)?; // Format into JSON string
+    let file_write_result = fs::write(response_path, json_contents); // Write string to JSON file
+    Ok(file_write_result?)
+}
+
+// Responses are redirected to a JSON file saved in subdirectoy '/output'
+// Construct the dir and return the Response file path to the dir
+fn build_file_path(path: PathBuf) -> Result<PathBuf> {
+    let parent = path.parent().expect("Failed to find parent directory!");
+    let out_dir = parent.join("output");
+    println!("{:?}, {:?}", parent, out_dir);
+    std::fs::create_dir_all(out_dir)?;
+    let filename = path.file_stem().expect("Could not retrieve file name!");
+    let ext = path.extension().expect("Couldn't retreive extension!");
+
+    let response_path_str = format!(
+        "{}/output/{}_response.{}",
+        parent.display(),
+        filename.to_str().unwrap(),
+        ext.to_str().unwrap()
+    );
+
+    let response_path = Path::new(&response_path_str).to_owned();
+    println!("{:?}", response_path); // DEBUG
+    Ok(response_path)
+}
+
+#[cfg(test)]
+mod test {
+    use std::path::Path;
+
+    use super::build_file_path;
+    #[test]
+    fn path_name() {
+        let resp_path = build_file_path(Path::new("/tests/test2.json").to_path_buf()).unwrap();
+
+        // Redundant but useful check
+        assert!(Path::new("/tests/output").try_exists().unwrap());
+        assert_eq!(resp_path, Path::new("/tests/test2_response.json"));
+    }
 }
